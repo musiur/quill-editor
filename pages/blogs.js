2335a20da07c1ec23f2b2cf9d11f08dbaa5e90aa
musiur <musiur.opu@gmail.com>
2023-04-04 13:48:52 +0600

    blogs data fetch and showcase done

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    const FetchBlogs = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/blogs")
+            if(response.status === 200){
+                setBlogs(response.data.result)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        FetchBlogs()
+    },[])
     return (
         <div>
-            Blogs
+            <h1 className="font-bold text-2xl text-center py-10">All Blogs</h1>
+            <div>
+                {
+                    blogs.length ? blogs.map((item) => {
+                        return (
+                            <div key={item.id}>
+                                <h2>{item.tile}</h2>
+                                <p>{item.description}</p>
+                                <div dangerouslySetInnerHTML={{ __html: item.body }} id="htmlPluggedIn" className="text-xl"/>
+                            </div>
+                        )
+                    })
+
+                    : null
+                }
+            </div>
         </div>
     )
 }
